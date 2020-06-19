127ca6401c8fb3252b7a724c8ddb7b15c419e2df
isievwore <isievwore@systemspecs.com.ng>
2020-06-19 02:14:49 +0100

    Updates

---
+++ precache-manifest.89495da47657a07363246aaa94df5add.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8b69c40dbf7f18cd02c629db4e1f25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9badb5b0150b02e763",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f0e28678c55ca8c535c0",
+    "url": "/static/css/main.f72bad04.chunk.css"
+  },
+  {
+    "revision": "4e9badb5b0150b02e763",
+    "url": "/static/js/2.daed8951.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.daed8951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e28678c55ca8c535c0",
+    "url": "/static/js/main.f244a26d.chunk.js"
+  },
+  {
+    "revision": "c47c325c67104dfb937c",
+    "url": "/static/js/runtime-main.1067d15c.js"
+  },
+  {
+    "revision": "76151a345b1824c555d9df66eb077643",
+    "url": "/static/media/bg-home.76151a34.jpg"
+  },
+  {
+    "revision": "29dfa06bef4c6d0af0340a1f07d1797d",
+    "url": "/static/media/kelvin-logo.29dfa06b.png"
+  },
+  {
+    "revision": "56a12d36b5cd84ec2ef6750866d07017",
+    "url": "/static/media/portfolio-1.56a12d36.png"
+  },
+  {
+    "revision": "7b2fd0e28ab9caac7bedc963a93cf2f0",
+    "url": "/static/media/portfolio-2.7b2fd0e2.png"
+  },
+  {
+    "revision": "46539c7326bbc5b6b567c21ca047730a",
+    "url": "/static/media/portfolio-3.46539c73.png"
+  },
+  {
+    "revision": "e4b8ff90c5985efe7215917fbb90051c",
+    "url": "/static/media/portfolio-4.e4b8ff90.png"
+  },
+  {
+    "revision": "b3a2019d45fb549ff23d81c4eeac6f40",
+    "url": "/static/media/portfolio-5.b3a2019d.png"
+  },
+  {
+    "revision": "a274b373022b4527e7d0f35afa44be96",
+    "url": "/static/media/portfolio-6.a274b373.png"
+  },
+  {
+    "revision": "5f65d4e449034474123d1502e94f76d8",
+    "url": "/static/media/portfolio-7.5f65d4e4.png"
+  },
+  {
+    "revision": "8517416204272646539cc688d4395a0a",
+    "url": "/static/media/portfolio-8.85174162.png"
+  },
+  {
+    "revision": "f539f591715d6761bd368e947c15680c",
+    "url": "/static/media/portfolio-9.f539f591.png"
+  },
+  {
+    "revision": "35a166cb88e5f5fb962ee305817890aa",
+    "url": "/static/media/profile-img.35a166cb.png"
+  }
+]);
